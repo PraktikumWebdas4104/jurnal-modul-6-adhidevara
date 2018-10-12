5db875762096a1327a12f07275b32eff8e4170bd
adhidevara <43514620+adhidevara@users.noreply.github.com>
2018-10-12 10:30:42 +0700

    Add files via upload

---
+++ akunmhs.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Okt 2018 pada 05.23
+-- Versi server: 10.1.34-MariaDB
+-- Versi PHP: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jurn6`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akunmhs`
+--
+
+CREATE TABLE `akunmhs` (
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `jk` varchar(100) NOT NULL,
+  `kelas` varchar(100) NOT NULL,
+  `fakultas` varchar(100) NOT NULL,
+  `hobi` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `akunmhs`
+--
+ALTER TABLE `akunmhs`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
